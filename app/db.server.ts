4bbfb93dd58d8a25b67b80307c39cea938b26735
Josh Rarick <rarick.joshua.e@gmail.com>
2024-04-10 23:27:32 -0500

    make db persist

--- app/db.server.ts
+++ app/db.server.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from '@prisma/client'
 import { singleton } from './singleton.server'
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  prisma = singleton('prisma', () => new PrismaClient())
-}
+const prisma = singleton('prisma', () => new PrismaClient())
 
 prisma.$connect()
 
