915be61c065d3df0d2d7f0cdc272e73c164991e5
Josh Rarick <rarick.joshua.e@gmail.com>
2024-04-10 23:25:48 -0500

    lets try this again

--- app/db.server.ts
+++ app/db.server.ts
@@ -3,7 +3,14 @@ import { PrismaClient } from '@prisma/client'
 import { singleton } from './singleton.server'
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const prisma = singleton('prisma', () => new PrismaClient())
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  prisma = singleton('prisma', () => new PrismaClient())
+}
+
 prisma.$connect()
 
 export { prisma }
